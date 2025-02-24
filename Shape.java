5fab259b4231b15237c61d2beeae0aa2d8536b03
Dani <dan.avila7@gmail.com>
2025-02-24 09:44:44 -0500

    vscode rules

--- Shape.java
+++ Shape.java
@@ -1,3 +1,7 @@
 public abstract class Shape {
-    public abstract void draw();  // Abstract method to enforce implementation in subclasses
+    public abstract void draw(); // Abstract method to enforce implementation in subclasses
+
+    public void resize(double factor) {
+        // Implement resizing logic here
+    }
 }
