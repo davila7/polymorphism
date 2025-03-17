0150cd4483b4fe403fd6f9c91e896e1a076fb300
Daniel Avila <dan.avila7@gmail.com>
2025-03-17 13:05:38 -0400

    Update Shape.java
    change get_description method

--- Shape.java
+++ Shape.java
@@ -1,7 +1,7 @@
 public abstract class Shape {
     public abstract void draw(); // Abstract method to enforce implementation in subclasses
     public abstract void erase(); // Añadir este método abstracto
-    public abstract String getDescription(); // Nuevo método abstracto
+    public abstract String get_description(); // Nuevo método abstracto
 
     public void resize(double factor) {
         // Implement resizing logic here
